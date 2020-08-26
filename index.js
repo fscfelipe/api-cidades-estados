b23df031ee86189c8e22d0350198f2ee52be6165
Felipe Sobreira <felipesobreira.c@hotmail.com>
2020-08-26 00:37:26 -0300

    lendo arquivos de estados

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 import express from 'express';
 
+import fs from 'fs';
+
 const app = express();
 const PORT = 3000;
+
+// Lendo arquivo de estados
+
+fs.readFile('./arquivos/Estados.json', 'utf8', (err, data) => {
+  let json = JSON.parse(data);
+  console.log(json);
+});
 
 app.get('/', (req, res) => {
   res.send('API funcionando!');
