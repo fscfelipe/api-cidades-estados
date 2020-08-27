06807a8d4d4fba58d42c17d5bb5f51c95ccc6651
Felipe Sobreira <felipesobreira.c@hotmail.com>
2020-08-26 22:55:19 -0300

    retornando uma lista com a quantidade de cidades por estados

--- index.js
+++ index.js
@@ -32,5 +32,25 @@ async function getQuantidadeCidades(estado) {
   return estadoJson.length;
 }
 
+async function getEstadosComMaisCidades(order) {
+  let estados = await fs.readFile('./arquivos/Estados.json', 'utf8');
+  let estadosJson = JSON.parse(estados);
+  let lista = [];
+
+  for (const estado of estadosJson) {
+    const num_cidades = await getQuantidadeCidades(estado.Sigla);
+
+    lista.push({ uf: estado.Sigla, num_cidades });
+  }
+
+  lista.sort((a, b) => {
+    if (order === 'desc') return b.num_cidades - a.num_cidades;
+    else return a.num_cidades - b.num_cidades;
+  });
+
+  return lista;
+}
+
 //createFiles();
-getQuantidadeCidades('CE');
+//getQuantidadeCidades('CE');
+//getEstadosComMaisCidades('desc');
